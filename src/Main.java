58142a13c53b04596c16dc7d6eebaa0b1c97d989
luan torres <luanvtorres@ucundinamarca.edu.co>
2025-08-28 23:09:39 -0500

    cambios

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        System.out.printf("HOLA MUNDO");
+        System.out.println("COMANDOS GIT");
+        System.out.println("git status  - estado rama");
+        System.out.println("git branch - ver las ramas");
+        System.out.println("git add - agrupar archivos");
+        System.out.println("git push origin - subir cambios");
+        System.out.println("git commit -m - guardar cambios");
+        System.out.println("git config -- list - listar configuracion");
+        System.out.println("git log");
+        System.out.println("git clone");
+        System.out.println("git fetch");
+        System.out.println("git switch");
+
+        System.out.println("git fectch -v - ramas remotas");
+        System.out.println("git switch -c - crear rama local");
+        System.out.println("git checkaut () - cambiar rama");
+    }
+}
